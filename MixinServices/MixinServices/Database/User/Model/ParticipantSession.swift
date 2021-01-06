2a67c2611d95f044fdde2e7f7d18350249966961
wuyueyang <wuyueyang@mixin.one>
2021-01-07 01:26:22 +0800

    Send and receive encrypted messages

--- MixinServices/MixinServices/Database/User/Model/ParticipantSession.swift
+++ MixinServices/MixinServices/Database/User/Model/ParticipantSession.swift
@@ -13,13 +13,16 @@ public struct ParticipantSession {
     public let sessionId: String
     public let sentToServer: Int?
     public let createdAt: String
+    public let publicKey: String?
     
-    public init(conversationId: String, userId: String, sessionId: String, sentToServer: Int?, createdAt: String) {
+    // FIXME: Remove that default value of publicKey
+    public init(conversationId: String, userId: String, sessionId: String, sentToServer: Int?, createdAt: String, publicKey: String? = nil) {
         self.conversationId = conversationId
         self.userId = userId
         self.sessionId = sessionId
         self.sentToServer = sentToServer
         self.createdAt = createdAt
+        self.publicKey = publicKey
     }
     
     public init(from decoder: Decoder) throws {
@@ -33,14 +36,59 @@ public struct ParticipantSession {
     
 }
 
-extension ParticipantSession: Codable, DatabaseColumnConvertible, MixinFetchableRecord, MixinEncodableRecord {
+extension ParticipantSession {
+    
+    public struct Sent: Codable, MixinFetchableRecord {
+        
+        public let conversationId: String
+        public let userId: String
+        public let sessionId: String
+        public let sentToServer: Int?
+        
+        public init(conversationId: String, userId: String, sessionId: String, sentToServer: Int?) {
+            self.conversationId = conversationId
+            self.userId = userId
+            self.sessionId = sessionId
+            self.sentToServer = sentToServer
+        }
+        
+        public var participantSession: ParticipantSession {
+            ParticipantSession(conversationId: conversationId,
+                               userId: userId,
+                               sessionId: sessionId,
+                               sentToServer: sentToServer,
+                               createdAt: Date().toUTCString())
+        }
+        
+    }
+    
+    public struct Key: Codable, MixinFetchableRecord {
+        
+        public let conversationId: String
+        public let userId: String
+        public let sessionId: String
+        public let publicKey: String?
+        
+        public init(conversationId: String, userId: String, sessionId: String, publicKey: String?) {
+            self.conversationId = conversationId
+            self.userId = userId
+            self.sessionId = sessionId
+            self.publicKey = publicKey
+        }
+        
+    }
+    
+}
 
+extension ParticipantSession: Codable, DatabaseColumnConvertible, MixinFetchableRecord, MixinEncodableRecord {
+    
     public enum CodingKeys: String, CodingKey {
         case conversationId = "conversation_id"
         case userId = "user_id"
         case sessionId = "session_id"
         case sentToServer = "sent_to_server"
         case createdAt = "created_at"
+        case publicKey = "public_key"
     }
     
 }
